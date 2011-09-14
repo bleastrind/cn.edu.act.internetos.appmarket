f3bf214604bb565580ac194ea1f1b9f8f72f94b0
bleastrind <bleastrind@gmail.com>
2011-09-14 09:09:03 +0800

    bug?

--- app/cn/edu/act/internetos/appmarket/service/AdminService.java
+++ app/cn/edu/act/internetos/appmarket/service/AdminService.java
@@ -5,26 +5,24 @@ import models.*;
 
 public class AdminService{
 
-
-
     public static void saveApp(App app, String name, String information){
-          AppDao appdao = new AppDao();
-          app.setName(name);
-          app.setInformation(information);
-		  app.setInstallUrl("TEST URL");
-          appdao.save(app);        
+	AppDao appdao = new AppDao();
+	app.setName(name);
+	app.setInformation(information);
+	app.setInstallUrl("TEST URL");
+	appdao.save(app);        
     }
 
     public static void deleteApp(App app){
-        AppDao appdao = new AppDao();
-		app.getId().equals("");
-        appdao.delete(app);
+	AppDao appdao = new AppDao();
+	if(!app.getId().equals(""))
+		appdao.delete(app);
     }
 
     public static void addAppSave(String name, String information, String installUrl)
     {
-          AppDao appdao = new AppDao();
-          App app = new App(name, information, installUrl);
-          appdao.save(app);     
+	AppDao appdao = new AppDao();
+	App app = new App(name, information, installUrl);
+	appdao.save(app);     
     }
 }
